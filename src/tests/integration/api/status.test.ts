cbf3462ddf5e2e395621893aaddb7eecc74f6cc7
Tim <randomthecoder1557@gmail.com>
2025-09-14 18:08:42 -0300

    refactor: move env var necessary for tests to separate file

--- src/tests/integration/api/status.test.ts
+++ src/tests/integration/api/status.test.ts
@@ -1,8 +1,9 @@
 import { describe, expect, test } from "vitest";
+import {BASE_URL} from "@/tests/constants"
 
 describe("GET /api/status", () => {
   test("should return 200", async () => {
-    const response = await fetch(`${process.env.BASE_URL}/api/status`);
+    const response = await fetch(`${BASE_URL}/api/status`);
 
     expect(response.status).toBe(200);
   });
