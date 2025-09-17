6be18b39e3c48b75b8adcc8de99efa6f7aab92be
Tim <randomthecoder1557@gmail.com>
2025-09-17 16:27:34 -0300

    test: add tests for `/api/status` route

--- src/tests/integration/api/status.test.ts
+++ src/tests/integration/api/status.test.ts
@@ -1,10 +1,24 @@
 import { describe, expect, test } from "vitest";
-import {BASE_URL} from "@/tests/constants"
+import { BASE_URL } from "@/tests/constants";
 
 describe("GET /api/status", () => {
   test("should return 200", async () => {
     const response = await fetch(`${BASE_URL}/api/status`);
 
     expect(response.status).toBe(200);
+  });
+
+  test("should be healthy", async () => {
+    const response = await fetch(`${BASE_URL}/api/status`);
+    const body = await response.json();
+
+    expect(body.status).toEqual("healthy");
+  });
+
+  test("should return correct database information", async () => {
+    const response = await fetch(`${BASE_URL}/api/status`);
+    const body = await response.json();
+
+    expect(body.db_version).toEqual("17.6 (Debian 17.6-1.pgdg13+1)");
   });
 });
