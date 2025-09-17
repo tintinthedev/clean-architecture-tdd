68abe66952f566895f2ad1f0e83d7ac156dd6999
Tim <randomthecoder1557@gmail.com>
2025-09-17 16:27:48 -0300

    feat: implement `/api/status` functionality

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,7 +1,20 @@
 import { NextResponse } from "next/server";
+import { db } from "@/db";
 
 export async function GET() {
-  return NextResponse.json({
-    status: "ok!",
-  });
+  try {
+    const dbVersionResult = await db.query("SHOW server_version");
+    const dbVersion = dbVersionResult.rows[0].server_version;
+
+    return NextResponse.json({
+      db_version: dbVersion,
+      status: "healthy",
+    });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json({
+      status: "unhealthy",
+    });
+  }
 }
