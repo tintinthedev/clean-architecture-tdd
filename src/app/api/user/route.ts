dbe5ff63e0a48bdbd3c49365bb89588784bced86
Tim <randomthecoder1557@gmail.com>
2025-09-18 15:40:36 -0300

    refactor: use `user` model for handling user creation

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,21 +1,10 @@
-import { db } from "@/db";
+import { userModel } from "@/models/user";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
-  const query = {
-    text: `INSERT INTO users(email, password) VALUES($1, $2)
-            RETURNING json_build_object(
-              'id', id,
-              'email', email
-            ) AS user`,
-    values: [body.email, body.password],
-  };
+  const user = await userModel.create(body.email, body.password);
 
-  const insertResult = await db.query(query);
-
-  const dbUser = insertResult.rows[0].user;
-
-  return NextResponse.json(dbUser);
+  return NextResponse.json(user);
 }
