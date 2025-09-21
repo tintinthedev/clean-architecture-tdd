da17c699dc177a994eb94c3dfa99026e3bfb805f
Tim <randomthecoder1557@gmail.com>
2025-09-21 17:00:00 -0300

    feat: store hashed password on database

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,14 +1,17 @@
 import { db } from "@/db";
+import { Password } from "./password";
 
 class User {
   static async create(email: string, password: string) {
+    const hashedPassword = await new Password(password).hash();
+
     const query = {
       text: `INSERT INTO users(email, password) VALUES ($1, $2)
               RETURNING json_build_object(
                 'id', id,
                 'email', email
               ) as user`,
-      values: [email, password],
+      values: [email, hashedPassword],
     };
 
     const queryResponse = await db.query(query);
