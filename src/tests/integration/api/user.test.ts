d7c2b819dc72b91403f2135150c68b7617b0aa12
Tim <randomthecoder1557@gmail.com>
2025-09-21 18:38:24 -0300

    test: add tests for data validation in user creation

--- src/tests/integration/api/user.test.ts
+++ src/tests/integration/api/user.test.ts
@@ -27,4 +27,90 @@ describe("POST /api/user", () => {
     // cleaning up
     await db.query(`DELETE FROM users`);
   });
+
+  test("should fail invalid email", async () => {
+    const userData = {
+      email: "invalid@gg",
+      password: "12345678",
+    };
+
+    const response = await fetch(`${BASE_URL}/api/user`, {
+      method: "POST",
+      body: JSON.stringify(userData),
+    });
+
+    expect(response.status).toBe(400);
+
+    const body = await response.json();
+
+    expect(body.error).not.toBe(undefined);
+    expect(Object.keys(body).length).toBe(1);
+  });
+
+  test("should fail invalid password", async () => {
+    const userData = {
+      email: "invalid@gmail.com",
+      password: "1234567",
+    };
+
+    const response = await fetch(`${BASE_URL}/api/user`, {
+      method: "POST",
+      body: JSON.stringify(userData),
+    });
+
+    expect(response.status).toBe(400);
+
+    const body = await response.json();
+
+    expect(body.error).not.toBe(undefined);
+    expect(Object.keys(body).length).toBe(1);
+  });
+
+  test("should fail empty data", async () => {
+    const userData = {};
+
+    const response = await fetch(`${BASE_URL}/api/user`, {
+      method: "POST",
+      body: JSON.stringify(userData),
+    });
+
+    expect(response.status).toBe(400);
+
+    const body = await response.json();
+
+    expect(body.error).not.toBe(undefined);
+    expect(Object.keys(body).length).toBe(1);
+  });
+
+  test("should fail no data", async () => {
+    const response = await fetch(`${BASE_URL}/api/user`, {
+      method: "POST",
+    });
+
+    expect(response.status).toBe(500);
+
+    const body = await response.json();
+
+    expect(body.error).not.toBe(undefined);
+    expect(Object.keys(body).length).toBe(1);
+  });
+
+  test("should fail invalid email and password", async () => {
+    const userData = {
+      email: "opa",
+      password: "seila",
+    };
+
+    const response = await fetch(`${BASE_URL}/api/user`, {
+      method: "POST",
+      body: JSON.stringify(userData),
+    });
+
+    expect(response.status).toBe(400);
+
+    const body = await response.json();
+
+    expect(body.error).not.toBe(undefined);
+    expect(Object.keys(body).length).toBe(1);
+  });
 });
